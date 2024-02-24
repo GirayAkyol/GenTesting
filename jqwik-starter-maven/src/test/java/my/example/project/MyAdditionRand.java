e8c86edcdd92e7e5e496bba5a3df075b741ad3dc
gitu <gitu@lgq>
2024-02-25 01:33:19 +0300

    fail-rand-assoc

--- jqwik-starter-maven/src/test/java/my/example/project/MyAdditionRand.java
+++ jqwik-starter-maven/src/test/java/my/example/project/MyAdditionRand.java
@@ -27,4 +27,16 @@ class MyAdditionRand {
         Assertions.assertThat(result1).isEqualTo(result2);
     }
 
+    @Example
+    void associative() {
+        int x = (int) (Math.random() * 100);
+        int y = (int) (Math.random() * 100);
+        int z = (int) (Math.random() * 100);
+
+        int result1 = new MyAddition().add(x, new MyAddition().add(y, z));
+        int result2 = new MyAddition().add(new MyAddition().add(x, y), z);
+
+        Assertions.assertThat(result1).isEqualTo(result2);
+    }
+
 }
