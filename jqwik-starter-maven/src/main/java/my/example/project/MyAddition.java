2b1c473e94c1e46a113dd1c5430456a06ac00601
gitu <gitu@lgq>
2024-02-25 01:26:36 +0300

    pass-rand-comm&assoc

--- jqwik-starter-maven/src/main/java/my/example/project/MyAddition.java
+++ jqwik-starter-maven/src/main/java/my/example/project/MyAddition.java
@@ -4,17 +4,7 @@ import java.util.*;
 
 public class MyAddition {
     public int add(int x, int y) {
-        if (x == -1 && y == 3) {
-            return 2;
-        } else if (x == 2 && y == 3) {
-            return 5;
-        } else if (x == 1 && y == 41) {
-            return 42;
-        }
-        if (x > y) {
-            return -7;
-        }
-        return 4;
+        return x * y;
     }
 }
 
