258885f78bb62c0a785cbaaddaf90fdf87a8c37b
gitu <gitu@lgq>
2024-04-18 17:25:51 +0300

    rle fixed

--- jqwik-starter-maven/src/main/java/my/example/project/MyRLE.java
+++ jqwik-starter-maven/src/main/java/my/example/project/MyRLE.java
@@ -7,8 +7,19 @@ import java.util.List;
 public class MyRLE {
     public static List<AbstractMap.SimpleEntry<Character, Integer>> rle(String inputStr) {
         List<AbstractMap.SimpleEntry<Character, Integer>> result = new ArrayList<>();
-        for (char ch : inputStr.toCharArray()) {
-            result.add(new AbstractMap.SimpleEntry<>(ch, 1));
+        if (inputStr == null || inputStr.isEmpty()) {
+            return result;
+        }
+        char currentChar = inputStr.charAt(0);
+        int currentCount = 1;
+        for (int i = 1; i < inputStr.length(); i++) {
+            if (inputStr.charAt(i) == currentChar) {
+                currentCount++;
+            } else {
+                result.add(new AbstractMap.SimpleEntry<>(currentChar, currentCount));
+                currentChar = inputStr.charAt(i);
+                currentCount = 1;
+            }
         }
         return result;
     }
