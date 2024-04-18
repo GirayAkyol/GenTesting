30983f428345464783a69740e58e6785c584c542
gitu <gitu@lgq>
2024-04-18 17:07:57 +0300

    product init

--- jqwik-starter-maven/src/test/java/my/example/project/MyMaxProductRand.java
+++ jqwik-starter-maven/src/test/java/my/example/project/MyMaxProductRand.java
@@ -22,31 +22,7 @@ public class MyMaxProductRand {
 
     void maxProduct2helper() {
         Random rand = new Random();
-
-        int size = rand.nextInt(10) + 2;
-        ArrayList<Integer> array = new ArrayList<Integer>();
-
-        for (int i = 0; i < size; i++) {
-            array.add(rand.nextInt(201) - 100);
-        }
-        MyMaxProduct myMaxProduct = new MyMaxProduct();
-        int result1 = myMaxProduct.maxProduct(array);
-        System.out.println(array);
-
-
-        int multiplier = rand.nextInt(201) - 100;
-        if (multiplier == 0) {
-            return;
-        }
-        System.out.println(multiplier);
-        for (int i = 0; i < array.size(); i++) {
-            array.set(i, array.get(i) * multiplier);
-        }
-
-        int result2 = myMaxProduct.maxProduct(array);
-        System.out.println(array);
-        Assertions.assertThat(result2 / multiplier / multiplier).isEqualTo(result1);
-
+        
 
     }
 
