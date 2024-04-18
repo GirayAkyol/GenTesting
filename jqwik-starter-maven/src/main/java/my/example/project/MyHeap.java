eeeddfd1731d376dd0ce7b6efea8e4ba28a54957
gitu <gitu@lgq>
2024-04-19 01:43:56 +0300

    heap usetype

--- jqwik-starter-maven/src/main/java/my/example/project/MyHeap.java
+++ jqwik-starter-maven/src/main/java/my/example/project/MyHeap.java
@@ -6,6 +6,12 @@ class MyHeap {
     final MyHeap left;
     final MyHeap right;
 
+    public MyHeap(int head) {
+        this.head = head;
+        this.left = null;
+        this.right = null;
+    }
+
     public MyHeap(int head, MyHeap left, MyHeap right) {
         this.head = head;
         this.left = left;
@@ -17,5 +23,16 @@ class MyHeap {
         String leftString = left == null ? "None" : left.toString();
         String rightString = right == null ? "None" : right.toString();
         return "{ " + head + ", " + leftString + ", " + rightString + " }";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyHeap myHeap = (MyHeap) o;
+        if (head != myHeap.head) return false;
+        boolean le = left != null ? left.equals(myHeap.left) : myHeap.left != null;
+        boolean re = right != null ? right.equals(myHeap.right) : myHeap.right != null;
+        return le && re;
     }
 }
--- jqwik-starter-maven/src/test/java/my/example/project/MyHeapProperties.java
+++ jqwik-starter-maven/src/test/java/my/example/project/MyHeapProperties.java
@@ -3,19 +3,19 @@ package my.example.project;
 import net.jqwik.api.Provide;
 import net.jqwik.api.*;
 import net.jqwik.api.arbitraries.*;
+import net.jqwik.api.constraints.UseType;
 import net.jqwik.api.lifecycle.*;
 import org.assertj.core.api.Assertions;
 
 public class MyHeapProperties {
 
     @Property
-    void test(@ForAll("heap") MyHeap a, @ForAll("heap") MyHeap b) {
+    @Report(Reporting.GENERATED)
+    void test(@ForAll @UseType MyHeap a, @ForAll @UseType MyHeap b) {
         Assertions.assertThat(isMinHeap(a)).isTrue();
         Assertions.assertThat(isMinHeap(b)).isTrue();
         MyHeap merged = mergeHeaps(a, b);
         Assertions.assertThat(isMinHeap(merged)).isTrue();
-        MyHeap merged2 = mergeHeaps2(a, b);
-        //Assertions.assertThat(merged2).isEqualTo(merged);
 
     }
 
@@ -47,44 +47,19 @@ public class MyHeapProperties {
         }
     }
 
-    private MyHeap mergeHeaps2(MyHeap h1, MyHeap h2) {
-        if (h1 == null) {
-            return h2;
-        }
-        if (h2 == null) {
-            return h1;
-        }
-        if (h1.head <= h2.head) {
-            return new MyHeap(h1.head, mergeHeaps(h1.left, h2), h1.right);
-        } else {
-            return new MyHeap(h2.head, mergeHeaps(h1, h2.right), h2.left);
-        }
-    }
+//    private MyHeap mergeHeaps2(MyHeap h1, MyHeap h2) {
+//        if (h1 == null) {
+//            return h2;
+//        }
+//        if (h2 == null) {
+//            return h1;
+//        }
+//        if (h1.head <= h2.head) {
+//            return new MyHeap(h1.head, mergeHeaps(h1.left, h2), h1.right);
+//        } else {
+//            return new MyHeap(h2.head, mergeHeaps(h1, h2.right), h2.left);
+//        }
+//    }
 
-    @Provide
-    Arbitrary<MyHeap> heap() {
-        Arbitrary<Integer> sizes = Arbitraries.integers().between(0, 10);
-        return sizes.flatMap(size -> heap(0, size));
-    }
-
-    private Arbitrary<MyHeap> heap(int minKey, int size) {
-        if (size == 0) {
-            return Arbitraries.just(null);
-        }
-        return Arbitraries.lazy(
-                () -> Arbitraries.frequencyOf(
-                        Tuple.of(1, Arbitraries.just(null)),
-                        Tuple.of(1, heapWithChildren(minKey, size))
-                )
-        );
-    }
-
-    private Arbitrary<MyHeap> heapWithChildren(int minKey, int size) {
-        Arbitrary<Integer> keys = Arbitraries.integers().greaterOrEqual(minKey);
-        return keys.flatMap(
-                head -> Combinators.combine(heap(head, size / 2), heap(head, size / 2))
-                        .as((left, right) -> new MyHeap(head, left, right))
-        );
-    }
 
 }
